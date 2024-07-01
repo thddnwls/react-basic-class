a9d6857c62044f8276ef1d883101ac9b0b6a8736
thddnwls <thddnwls0831@daum.net>
2024-07-01 14:17:18 +0900

    react-movie-app

---
+++ src/routes/home/Home.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import Movie from '../../components/movie/Movie';
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // prettier-ignore
+    (async () => {
+      const res = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json();
+      setMovies(res.data.movies);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            coverImage={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Home;
