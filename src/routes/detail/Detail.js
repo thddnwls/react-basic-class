758aa2f1facbd8de2daebfd9908552c23bc81b3e
thddnwls <thddnwls0831@daum.net>
2024-07-03 16:31:07 +0900

    react-movie-app

--- src/routes/detail/Detail.js
+++ src/routes/detail/Detail.js
@@ -1,17 +1,39 @@
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import styles from './detail.module.css';
+import { GoArrowLeft } from 'react-icons/go';
 
 function Detail() {
   const { id } = useParams();
+  const history = useHistory();
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // prettier-ignore
     (async () => {
       const res = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+      setData(res.data.movie);
+      setLoading(false)
     })();
   }, []);
 
-  return <h1>Detail</h1>;
+  return (
+    <div className={styles.content_wrap}>
+      {loading ? (
+        <h1>loading</h1>
+      ) : (
+        <>
+          <button onClick={history.goBack} className={styles.content_prev}>
+            <GoArrowLeft />
+          </button>
+
+          <h1>{data.title}</h1>
+          <p>{data.description_intro}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
